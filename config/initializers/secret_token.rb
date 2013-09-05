46bc0d1a9a20e196c65dd661ac9e592ec4bade4d
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2013-09-05 01:25:07 -0600

    Initial commit of demo_app.
    demo_app is the second Ruby on Rails application for the CSCI 3308 class.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '78f25426bac606ce1f408fc840bdd11adf35e67fcedf55661c8e948bd7a3dc418670b11581cef021a4e5c7148a77e02e8a3363a89a86032af1ea9e19d6f83c86'
